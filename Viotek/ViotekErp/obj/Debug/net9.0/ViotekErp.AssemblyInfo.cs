51144218e69873601cf3c98de2b18c3cd5ad2abe
Ahmet Kaya <academyakm0@gmail.com>
2025-12-19 16:02:20 +0300

    Sunucuya bağlandı

--- Viotek/ViotekErp/obj/Debug/net9.0/ViotekErp.AssemblyInfo.cs
+++ Viotek/ViotekErp/obj/Debug/net9.0/ViotekErp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViotekErp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68360158462637ef05ddaa8f162979df6bc3aa7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViotekErp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViotekErp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
